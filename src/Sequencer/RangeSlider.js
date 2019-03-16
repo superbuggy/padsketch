2ddd30b6c302cf36ae4d1ad2620b85e37a3f1291
James Reichard <jpqreichard@gmail.com>
2019-03-16 13:33:46 -0400

    Generalize Slider

---
+++ src/Sequencer/RangeSlider.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function RangeSlider({ min, max, handleChange, value, label }) {
+
+  const onChange = event => {
+    event.persist()
+    handleChange(parseInt(event.target.value, 10))
+  }
+
+  return (
+    <div>
+    <label>
+      {label}
+      <input 
+        type="range"
+        min={min}
+        max={max}
+        step={1}
+        onChange={onChange} 
+        defaultValue={value}
+        />
+      {value}
+    </label>
+    </div>
+  )
+
+}
