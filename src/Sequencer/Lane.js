1a0762a09554b95d286af4fa29d674d3d47519ab
James Reichard <jpqreichard@gmail.com>
2019-03-15 23:24:30 -0400

    Add sequence generating utils

--- src/Sequencer/Lane.js
+++ src/Sequencer/Lane.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import {generateSequence, rotate} from './utils'
 
 export default function Lane ({steps, instrument, ...props}) {
+
   const stepsPerInstrument = steps.map(step => (
     <div
       key={step}
       className="step"
     />
-
   ))
+
   return (
     <div className="instrument-lane">
       { stepsPerInstrument }
---
+++ src/Sequencer/utils.js
@@ -0,0 +1,74 @@
+// Bjorklund algorithm from https://gist.github.com/withakay/1286731
+
+/*
+An implementation of the Bjorklund algorithm in JavaScript
+Inspired by the paper 'The Euclidean Algorithm Generates Traditional Musical Rhythms'
+by Godfried Toussaint
+
+This is a port of the original algorithm by E. Bjorklund which I
+found in the paper 'The Theory of Rep-Rate Pattern Generation in the SNS Timing Systems' by
+E. Bjorklund.
+Jack Rutherford
+*/
+
+export function generatePattern (pulses, steps) { // renamed from the original
+  steps = Math.round(steps)
+  pulses = Math.round(pulses)
+
+  if (pulses > steps || pulses === 0 || steps === 0) {
+    return []
+  }
+
+  let pattern = []
+  let counts = []
+  let remainders = []
+  let divisor = steps - pulses
+  remainders.push(pulses)
+  let level = 0
+
+  while (true) {
+    counts.push(Math.floor(divisor / remainders[level]))
+    remainders.push(divisor % remainders[level])
+    divisor = remainders[level]
+    level += 1
+    if (remainders[level] <= 1) {
+      break
+    }
+  }
+
+  counts.push(divisor)
+
+  const builder = () => {
+    let r = 0
+    return function build (level) {
+      r += 1
+      if (level > -1) {
+        for (var i = 0; i < counts[level]; i++) {
+          build(level - 1)
+        }
+        if (remainders[level] !== 0) {
+          build(level - 2)
+        }
+      } else if (level === -1) {
+        pattern.push(0)
+      } else if (level === -2) {
+        pattern.push(1)
+      }
+    }
+  }
+
+  builder()(level)
+  return pattern.reverse()
+}
+
+export function rotate (array, times) {
+  if (!array.length) return []
+  if (!times) return array
+  let rotatedArray = array.slice()
+  let count = 0
+  while (count < times) {
+    rotatedArray.push(rotatedArray.shift()) //rotate left
+    count += 1
+  }
+  return rotatedArray
+}
